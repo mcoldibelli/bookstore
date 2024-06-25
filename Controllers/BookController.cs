4b33e0e29a7c907a100f940d8542e52f91f0243a
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-06-25 12:11:44 -0300

    feat: add controllers

---
+++ Controllers/BookController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookstore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookController : ControllerBase
+{
+  [HttpGet]
+  public IActionResult GetAll()
+  {
+    try
+    {
+      return Ok();
+    }
+    catch (Exception ex)
+    {
+      return BadRequest(new { ex.Message });
+    }
+  }
+
+  [HttpPost]
+  public IActionResult Add()
+  {
+    try
+    {
+      return Ok();
+    }
+    catch (Exception ex)
+    {
+      return BadRequest(new { ex.Message });
+    }
+  }
+
+  [HttpPut("{id}")]
+  public IActionResult Update()
+  {
+    try
+    {
+      return Ok();
+    }
+    catch (Exception ex)
+    {
+      return BadRequest(new { ex.Message });
+    }
+  }
+
+  [HttpDelete("{id}")]
+  public IActionResult Delete()
+  {
+    try
+    {
+      return Ok();
+    }
+    catch (Exception ex)
+    {
+      return BadRequest(new { ex.Message });
+    }
+  }
+}

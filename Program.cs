5484ddc1ded83a908152c5332957fdadb3e1083c
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-06-25 18:05:21 -0300

    feat: add repository and avoid cycles

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
+using System.Text.Json.Serialization;
 using bookstore.Context;
 using bookstore.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DatabaseContext>();
 builder.Services.AddScoped<IDatabaseContext, DatabaseContext>();
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
